428d8ca21cf63f2672a64923273739137cbb4a78
Ryan Beckett <32848753+Pang@users.noreply.github.com>
2026-02-23 16:08:22 +0000

    main header

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,4 @@
-#include <SFML/Graphics.hpp>
-#include <SFML/Audio.hpp>
-#include <fftw3.h>
-
-#include <vector>
-#include <cmath>
-#include <iostream>
-#include <cstdint>
-
-#include "SphereVisualiser/SphereVisualiser.h"
-#include "FftwService/FftwService.h"
-
-const int FFT_SIZE = 1024;
-const int BAR_COUNT = 360;
+#include "main.h"
 
 int main()
 {
